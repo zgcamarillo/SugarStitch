2ecec48d6a3b8b976f5ea4b5693d4860e7fb53bc
Zayra Guerrero Camarillo <zayraadielencamarillo@gmail.com>
2026-03-19 13:31:35 -0400

    fix: remove .env file from repository and add to gitignore

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,64 @@
 // Auth 
 
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import api from '../services/api'
+
 export default function Login() {
+    const navigate = useNavigate()
+
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    })
+
+    const [message, setMessage] = useState('')
+    const [error, setError] = useState('')
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        })
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        setMessage('')
+        setError('')
+
+        try {
+            const response = await api.post('/auth/login', formData)
+
+            localStorage.setItem('token', response.data.token)
+            localStorage.setItem('user', JSON.stringify(response.data.user))
+
+            setMessage(response.data.message)
+
+            setTimeout(() => {
+                navigate('/account')
+            }, 1000)
+        } catch (err) {
+            setError(err.response?.data?.message || 'Something went wrong')
+        }
+    }
+
+
     return (
-        <h1>Login</h1>
+        <div>
+            <h1>LOGIN</h1>
+
+            <form onSubmit={handleSubmit}>
+                <input type="email" name="email" placeholder="Email" calue={formData.email} onChange={handleChange} required />
+                <br></br>
+                <input type="password" name="password" placeholder="Password" value={formData.password} onChange={handleChange} required />
+                <br></br>
+
+                <button type="submit">Login</button>
+            </form>
+
+            {message && <p>{message}</p>}
+            {error && <p>{error}</p>}
+        </div>
     )
 }
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,97 @@
 // Auth
 
-export default function Register() {
+import { useState } from 'react'
+import api from '../services/api'
+
+function Register() {
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName: '',
+        age: '',
+        phoneNumber: '',
+        email: '',
+        password: '',
+        expertise: 'beginner',
+        allergies: '',
+        measurements: '',
+    })
+
+    const [message, setMessage] = useState('')
+    const [error, setError] = useState('')
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        })
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault() 
+        setMessage('')
+        setError('')
+
+        try {
+            const payload = {
+                ...formData,
+                age: Number(formData.age),
+                allergies: formData.allergies ? formData.allergies.split(',').map((item) => item.trim()) : [],
+            }
+
+            const response = await api.post('/auth/register', payload)
+
+            setMessage(response.data.message)
+
+            setFormData({
+                firstName: '',
+                lastName: '',
+                age: '',
+                phoneNumber: '',
+                email: '',
+                password: '',
+                expertise: 'beginner',
+                allergies: '',
+                measurements:'',
+            })
+        } catch (err) {
+            setError(err.response?.data?.message || 'Something went wrong')
+        }
+    }
     return (
-        <h1>THIS IS WHERE YOU REGISTER</h1>
+        <div>
+            <h1>REGISTER</h1>
+
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="firstName" placeholder="First Name" value={formData.firstName} onChange={handleChange} require/>
+                <br></br>
+                <input type="text" name="lastName" placeholder="Last Name" value={formData.lastName} onChange={handleChange} require/>
+                <br></br>
+                <input type="number" name="age" placeholder="Age" value={formData.age} onChange={handleChange} require/>
+                <br></br>
+                <input type="text" name="phoneNumber" placeholder="Phone Number" value={formData.phoneNumber} onChange={handleChange} require/>
+                <br></br>
+                <input type="email" name="email" placeholder="Email" value={formData.email} onChange={handleChange} require/>
+                <br></br>
+                <input type="password" name="password" placeholder="Password" value={formData.password} onChange={handleChange} require/>
+                <br></br>
+                <select name="expertise" value={formData.expertise} onChange={handleChange}>
+                    <option value="beginner">Beginner</option>
+                    <option value="intermediate">Intermediate</option>
+                    <option value="advanced">Advanced</option>
+                </select>
+                <br></br>
+                <input type="text" name="allergies" placeholder="Allergies (comma separated)" value={formData.allergies} onChange={handleChange} />
+                <br></br>
+                <input type="text" name="measurements" placeholder="Measurements (optional)" value={formData.measurements} onChange={handleChange} />
+                <br></br>
+                <button type="submit">Create Account</button>
+            </form>
+
+            {message && <p>{message}</p>}
+            {error && <p>{error}</p>}
+
+        </div>
     )
 }
+
+export default Register
