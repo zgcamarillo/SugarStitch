927615c10257a812bbb9a669b453163aa0d62f6a
Zayra Guerrero Camarillo <zayraadielencamarillo@gmail.com>
2026-03-19 08:32:08 -0400

    feat: build backend auth with MongoDB, user model, register, and login routes

---
+++ server/controllers/authControllers.js
@@ -0,0 +1,92 @@
+const bcrypt = require('bcryptjs')
+const User = require('../models/User')
+const jwt = require('jsonwebtoken')
+
+const registerUser = async (req, res) => {
+    try {
+        const {
+            firstName,
+            lastName,
+            age,
+            phoneNumber,
+            email,
+            password,
+            expertise,
+            allergies,
+            measurements,
+        } = req.body
+
+        const existingUser = await User.findOne({ email })
+
+        if (existingUser) {
+            return res.status(400).json({ message: 'User already exists' })
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        const newUser = await User.create({
+            firstName,
+            lastName,
+            age,
+            phoneNumber,
+            email,
+            password: hashedPassword,
+            expertise,
+            allergies,
+            measurements,
+        })
+
+        res.status(201).json({
+            message: 'User registered successsfully',
+            user: {
+                id: newUser._id,
+                firstName: newUser.firstName,
+                lastName: newUser.lastName,
+                email: newUser.email,
+                expertise: newUser.expertise,
+                role: newUser.role,
+            },
+        })
+    } catch (error) {
+        res.status(500).json({ message: error.message})
+    }
+}
+ const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '7d',
+    })
+ }
+
+ const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body 
+
+        const user = await User.findOne({ email })
+
+        if(!user) {
+            return res.status(400).json({ message: 'Invalid credentials' }) 
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Invalid credentialss' })
+        }
+
+        res.json({ 
+            message: 'Login successful',
+            token: generateToken(user._id),
+            user: {
+                id: user._id,
+                firstName: user.firstName,
+                email: user.email,
+                role: user.role,
+            },
+        })
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+ }
+
+
+module.exports = { registerUser, loginUser }
---
+++ server/models/User.js
@@ -0,0 +1,83 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        lastName: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        age: {
+            type: Number,
+            required: true,
+            min: 16,
+        },
+        phoneNumber:{ 
+            type: String,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            lowercase: true,
+        },
+        password: {
+            type: String,
+            require: true,
+        },
+        expertise: {
+            type: String,
+            enum: ['beginner', 'intermediate', 'advanced'],
+            required: true,
+        },
+        allergies: {
+            type: [String],
+            default: [],
+        },
+        measurements: {
+            type: String,
+            default: '',
+            trim: true,
+        },
+        role: {
+            type: String,
+            enum: ['user', 'admin'],
+            default: 'user'
+        },
+        xp: {
+            type: Number, 
+            default: 0,
+        },
+        level: {
+            type: Number,
+            default: 1,
+        },
+        charms: {
+            type: [String],
+            default: [],
+        },
+        crochetDay: {
+            type: [String],
+            default: [],
+        },
+        weeklyGoal:  {
+            type: Number,
+            default: 0,
+        },
+        dailyGoal: {
+            type: Number, 
+            default: 0,
+        },
+    },
+    { timestamps: true }
+)
+
+module.exports = mongoose.model('User', userSchema)
