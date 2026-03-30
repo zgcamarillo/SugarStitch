17972ec48f8a03f43fed0eb7538b8fdcbc3e22a4
Zayra Guerrero Camarillo <zayraadielencamarillo@gmail.com>
2026-03-30 17:50:07 -0400

    Fix: redeployment

--- client/src/api.js
+++ client/src/api.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
+console.log('API URL:', import.meta.env.VITE_API_URL)
+
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000',
 })
